2bc39753d8abd72cd98c2559bde0f45bd3e735e8
samuel <saa2280@cumc.columbia.edu>
2024-11-13 21:32:49 +0300

    outcome config

---
+++ sql_queries/Tx_Curr_OutComeList.sql
@@ -0,0 +1,4 @@
+INSERT INTO Tx_Curr_OutComeList (Region, Woreda, Facility, HMISCode, ReportYear, ReportMonth, TOs ,Losts,Drops,Deads,Stops,Not_Updated,Traced_Back,Restarts,TI,New) VALUES (
+ .Range("S" & 2).Value & "','" & .Range("T" & 2).Value & "','" & .Range("U" & 2).Value & "','" & .Range("V" & 2).Value & "','"
+& .Range("W" & 2).Value & "','" & .Range("X" & 2).Value & "','" & .Range("O" & 2).Value & "','" & .Range("P" & 2).Value & "','"
+    & .Range("Q" & 2).Value & "','" & .Range("R" & 2
